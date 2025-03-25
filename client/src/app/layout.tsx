15181f397d16cf52b3178c1d54920152d8260cf7
sabaimran <narmiabas@gmail.com>
2025-03-25 16:58:28 -0700

    add header with sidebar inset (but it messes up the screen height of other components)

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppSidebar } from "@/components/AppSidebar";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -31,8 +32,18 @@ export default function RootLayout({
 			>
 				<SidebarProvider>
 					<AppSidebar />
-					<SidebarTrigger />
-					{children}
+					<SidebarInset>
+						<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+							<SidebarTrigger className="-ml-1" />
+							<Separator orientation="vertical" className="mr-2 h-4" />
+							<header>
+								<h1 className="text-2xl font-bold">
+									The Annotated Paper
+								</h1>
+							</header>
+						</header>
+						{children}
+					</SidebarInset>
 				</SidebarProvider>
 			</body>
 		</html>
