55f33f0171b836a29df966ac53f64dc41c2efb31
sabaimran <narmiabas@gmail.com>
2025-04-18 16:24:38 -0700

    remove unused imports in dropdown menu

--- client/src/app/paper/[id]/page.tsx
+++ client/src/app/paper/[id]/page.tsx
@@ -43,8 +43,6 @@ import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
