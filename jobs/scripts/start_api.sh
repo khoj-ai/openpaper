80ca170f4f05a0331cd3fd6641b9dd14043c8235
sabaimran <narmiabas@gmail.com>
2025-06-25 12:48:55 -0700

    remove reload flag

--- jobs/scripts/start_api.sh
+++ jobs/scripts/start_api.sh
@@ -2,4 +2,4 @@
 
 # Start FastAPI application
 echo "Starting FastAPI application..."
-python -m uvicorn src.app:app --host 0.0.0.0 --port 8001 --reload --log-level info
+python -m uvicorn src.app:app --host 0.0.0.0 --port 8001 --log-level info
