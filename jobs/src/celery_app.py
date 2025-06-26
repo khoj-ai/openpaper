659e455ac00ad43e97d1807bc99acfb641f8458a
sabaimran <narmiabas@gmail.com>
2025-06-26 11:40:49 -0700

    use ssl.CERT_REQUIRED when using rediss
    this is required for encrypted traffic to backend redis instances

--- jobs/src/celery_app.py
+++ jobs/src/celery_app.py
@@ -2,16 +2,28 @@
 Celery application configuration and setup.
 """
 import os
+import ssl
 from dotenv import load_dotenv
 from celery import Celery # type: ignore
+
+
+BROKER_URL = os.getenv("CELERY_BROKER_URL", "pyamqp://guest@localhost:5672//")
+BACKEND_URL = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
 
 # Create Celery instance
 celery_app = Celery(
     "openpaper_tasks",
-    broker=os.getenv("CELERY_BROKER_URL", "pyamqp://guest@localhost:5672//"),
-    backend=os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0"),
+    broker=BROKER_URL,
+    backend=BACKEND_URL,
     include=["src.tasks"]
 )
+
+transport_options = {}
+
+if BACKEND_URL.startswith("rediss://"):
+    transport_options.update({
+        "ssl_cert_reqs": ssl.CERT_REQUIRED
+    })
 
 load_dotenv()  # Load environment variables from .env file
 
@@ -20,6 +32,7 @@ celery_app.conf.update(
     task_serializer="json",
     accept_content=["json"],
     result_serializer="json",
+    result_backend_transport_options=transport_options,
     timezone="UTC",
     enable_utc=True,
     result_expires=3600,  # Results expire after 1 hour
@@ -30,6 +43,8 @@ celery_app.conf.update(
     task_acks_late=True,
     worker_max_tasks_per_child=1000,
 )
+
+celery_app.autodiscover_tasks()
 
 if __name__ == "__main__":
     celery_app.start()
