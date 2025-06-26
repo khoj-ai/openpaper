552c95e09f442cbe088630623edbd4c7b405e791
sabaimran <narmiabas@gmail.com>
2025-06-25 23:45:19 -0700

    sync start script with local script

--- jobs/scripts/start_worker.sh
+++ jobs/scripts/start_worker.sh
@@ -3,4 +3,4 @@
 # Start Celery worker
 echo "Starting Celery worker..."
 source .venv/bin/activate
-python -m celery --app src.celery_app worker --loglevel=info --queues=pdf_processing --concurrency=1
+python -m celery --app src.celery_app worker --loglevel=info
