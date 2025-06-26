6b424cb82269692827274d2c3dcab40ec9c3d2da
sabaimran <narmiabas@gmail.com>
2025-06-25 20:36:15 -0700

    add environment activation step
    activate the environment for the celery worker

--- jobs/scripts/start_worker.sh
+++ jobs/scripts/start_worker.sh
@@ -2,4 +2,5 @@
 
 # Start Celery worker
 echo "Starting Celery worker..."
+source .venv/bin/activate
 python -m celery --app src.celery_app worker --loglevel=info --queues=pdf_processing --concurrency=1
