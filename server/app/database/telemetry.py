893fcf65f99d504b16b31cecfcfeaadc74c8e9c1
saba imran <narmiabas@gmail.com>
2025-12-22 19:02:10 -0800

    add subscription status to telemetry data

--- server/app/database/telemetry.py
+++ server/app/database/telemetry.py
@@ -1,5 +1,7 @@
 import os
 
+from app.database.crud.subscription_crud import subscription_crud
+from app.database.database import get_db
 from posthog import Posthog
 
 POSTHOG_API_KEY = os.getenv("POSTHOG_API_KEY", None)
@@ -22,7 +24,7 @@ if DEBUG:
     posthog.debug = True
 
 
-def track_event(event_name, properties=None, user_id=None, sync=False):
+def track_event(event_name, properties={}, user_id=None, sync=False):
     """
     Track an event with PostHog.
 
@@ -30,16 +32,35 @@ def track_event(event_name, properties=None, user_id=None, sync=False):
     :param properties: Optional dictionary of properties to associate with the event.
     """
     if POSTHOG_API_KEY and not DEBUG:
+        subscription = None
         if user_id is None:
             user_id = "anonymous"
+        else:
+            db = next(get_db())
+            subscription = subscription_crud.get_by_user_id(db, user_id=user_id)
+
+            if subscription:
+                properties.update(
+                    {
+                        "subscription_plan": subscription.plan,
+                        "subscription_status": subscription.status,
+                    }
+                )
+            else:
+                properties.update(
+                    {
+                        "subscription_plan": None,
+                        "subscription_status": None,
+                    }
+                )
 
         if sync:
             posthog_sync.capture(
-                distinct_id=user_id, event=event_name, properties=properties or {}
+                distinct_id=user_id, event=event_name, properties=properties
             )
         else:
             posthog.capture(
-                distinct_id=user_id, event=event_name, properties=properties or {}
+                distinct_id=user_id, event=event_name, properties=properties
             )
     else:
         print(
