4c1732845aab317595edf07c552fcc2c9efec3d8
saba imran <narmiabas@gmail.com>
2025-12-29 11:04:33 -0800

    add googletagmanager to main layout
    need this for conversion tracking :( sad.

--- client/src/app/(main)/layout.tsx
+++ client/src/app/(main)/layout.tsx
@@ -10,6 +10,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { PostHogProvider, ThemeProvider } from "@/lib/providers";
 import { SidebarController } from "@/components/utils/SidebarAutoCollapse";
 import Image from "next/image";
+import Script from "next/script";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -72,6 +73,18 @@ export default function RootLayout({
 					}}
 				/>
 				<script defer data-domain="openpaper.ai" src="https://plausible.io/js/script.js"></script>
+				<Script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=AW-17815378235"
+				/>
+				<Script id="google-analytics">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'AW-17815378235');
+					`}
+				</Script>
 			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
