d6c9f0214ba89a0e1b6d1516d7f9738cf316dc65
saba imran <narmiabas@gmail.com>
2026-01-08 12:19:19 -0800

    add in task_acks_late=False to prevent waiting for job ocmpletion to ack tasks
    this is important because it helps the broker know to wait for task
    acknowledgement
    
    reject_on_worker_lost allows the task to be rescheduled if the task
    times out/disappears without a final response

--- jobs/src/celery_app.py
+++ jobs/src/celery_app.py
@@ -30,7 +30,8 @@ celery_app.conf.update(
         "upload_and_process_file": {"queue": "pdf_processing"}
     },
     worker_prefetch_multiplier=1,  # Process one task at a time
-    task_acks_late=True,
+    task_acks_late=False,
+    reject_on_worker_lost=True,
     worker_max_tasks_per_child=1000,
     # Health monitoring settings
     worker_send_task_events=True,
