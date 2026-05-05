6c02d37e5431636327a1ff7a758574e50629f58e
saba imran <narmiabas@gmail.com>
2026-05-04 17:41:56 -0700

    s/pypdf2/pypdf

--- server/app/helpers/parser.py
+++ server/app/helpers/parser.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from typing import Tuple
 
 import requests
-from PyPDF2 import PdfReader
+from pypdf import PdfReader
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
